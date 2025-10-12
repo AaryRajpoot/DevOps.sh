2c3a7c8aefde71bc08ec717791cf8b754800417c
AaryRajpoot <ronit1336@gmail.com>
2025-10-12 10:01:36 +0530

    Update jenkins1.sh

--- jenkins1.sh
+++ jenkins1.sh
@@ -1,31 +1,16 @@
 # Add Jenkins repository
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-# Import Jenkins key
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-# Update system
 sudo dnf update -y
-
-# Install dependencies
 sudo dnf install fontconfig java-17-amazon-corretto -y
-
 # Install Jenkins
 sudo dnf install jenkins -y
-
-# Reload systemctl daemon
 sudo systemctl daemon-reload
-
-# Enable and start Jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-
-# Check Jenkins status
 sudo systemctl status jenkins
-
+jenkins --version
 # Install Git (no service needed)
 sudo dnf install git -y
-
-# Check Git version
 git --version
